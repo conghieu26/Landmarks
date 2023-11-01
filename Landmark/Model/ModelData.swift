f8c85412e235758e077a2f365f29180c649420a8
conghieu26 <hieunc00@gmail.com>
2023-11-01 21:23:03 +0700

    Create a landmark model

--- Landmark/Model/ModelData.swift
+++ Landmark/Model/ModelData.swift
@@ -5,16 +5,30 @@
 //  Created by CÔNG HIẾU on 01/11/2023.
 //
 
-import SwiftUI
+import Foundation
 
-struct ModelData: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
     }
-}
 
-struct ModelData_Previews: PreviewProvider {
-    static var previews: some View {
-        ModelData()
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
