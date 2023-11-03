143a69012af30090559f4318319b75da10271875
conghieu26 <hieunc00@gmail.com>
2023-11-03 15:36:36 +0700

    Change some detail information of region

--- Landmark/Views/ContentView.swift
+++ Landmark/Views/ContentView.swift
@@ -9,30 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height:300)
-            CircleImage()
-                .offset(y: -130)
-            
-            VStack(alignment: .leading){
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack{
-                    Text("Joshua Tree Nation Park")
-                    Spacer()
-                    Text("California")
-                }
-                .foregroundStyle(.secondary)
-                Divider()
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("DesCriptive text goes here")
-                    .font(.subheadline)
-            }
-            .padding()
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
