a2eff5ce1048b45c6c3fdbcd6d4fc9abc1676e64
conghieu26 <hieunc00@gmail.com>
2026-03-22 21:39:11 +0700

    Add a category view

---
+++ Landmark/Views/CategoryHome.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryHome.swift
+//  Landmark
+//
+//  Created by Nguyen Cong Hieu on 22/3/26.
+//
+
+import SwiftUI
+
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationSplitView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+
+#Preview {
+    CategoryHome()
+}
